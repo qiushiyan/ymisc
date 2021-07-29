5e893783013a36028f213144129b9739c74446a7
qiushi <qiushi.yann@gmail.com>
2021-07-30 00:33:46 +0800

    update code_blocks

--- R/code_blocks.R
+++ R/code_blocks.R
@@ -1,8 +1,9 @@
 #' Convert scripts into R Markdown code blocks
 #'
 #' intended to use with chunk option results='asis'
-#' @param dirs the directory containing scripts
-#'
+#' @param dirs directories containing scripts
+#' @param exclude files or directories to exclude
+#' @param print_tree whether file structure should be printed
 #' @return
 #' @export
 #'
@@ -11,14 +12,40 @@
 #' # code_blocks(c(dir1, dir2, ...))
 #' # convert single script
 #' # code_block(file)
-code_blocks <- function(dirs, exclude = NULL) {
+code_blocks <- function(dirs, exclude = NULL, print_tree = TRUE) {
+  # print file structure
+  cat("```\n")
   for (dir in dirs) {
-    files <- fs::dir_ls(dir)
-    for (file in files) {
-      if (file %in% exclude) {
-        next
+    if (dir %in% exclude) {
+      next
+    }
+    is_dir <- !utils::file_test("-f", dir)
+    if (print_tree && is_dir) {
+      cat(fs::dir_tree(dir), sep = "\n")
+    } else {
+      cat(dir, sep = "\n")
+    }
+
+  }
+  cat("```\n")
+
+  # print file contents
+  for (dir in dirs) {
+    is_dir <- !utils::file_test("-f", dir)
+    if (is_dir) {
+      files <- fs::dir_ls(dir)
+      for (file in files) {
+        if (file %in% exclude) {
+          next
+        }
+        if (!utils::file_test("-f", file)) {
+          code_blocks(file, print_tree = FALSE)
+        } else {
+          code_block(file)
+        }
       }
-      code_block(file)
+    } else {
+      code_block(file = dir)
     }
     cat("\n")
   }
@@ -28,27 +55,24 @@ code_blocks <- function(dirs, exclude = NULL) {
 #' @rdname code_blacks
 #' @usage NULL
 code_block <- function(file) {
-  block_type <- function(file) {
-    ext = tools::file_ext(file)
-    switch(ext,
-           "r" =".r",
-           "R" = ".r",
-           "py" = ".python",
-           "js" = ".javascript",
-           "ts" = ".javascript",
-           "jsx" = ".javascriptreact",
-           "tsx" = "javascriptreact",
-           ""
-    )
-  }
-
-  type = block_type(file)
-  contents = readLines(file)
-  cat(glue("```{<type>}", .open = "<", .close = ">"), sep = "\n")
-  cat(glue("# {file}"), sep = "\n")
+  ext <- tools::file_ext(file)
+  type <- switch(ext,
+         "r" = list(language = ".r", comment = "#"),
+         "R" = list(language = ".r", comment = "#"),
+         "Rmd" = list(language = ".rmarkdown", comment = "#"),
+         "py" = list(language = ".python", comment = "#"),
+         "js" = list(language = ".javascript", comment = "//"),
+         "ts" = list(language = ".javascript", comment = "//"),
+         "jsx" = list(language = ".javascriptreact", comment = "//"),
+         "tsx" = list(language = ".javascriptreact", comment = "//"),
+         list(language = "", comment = "#")
+  )
+  contents <- readLines(file)
+  cat(glue::glue("```{<type$language>}", .open = "<", .close = ">"), sep = "\n")
+  cat(glue::glue("{type$comment} {file}"), sep = "\n")
   for (line in contents) {
     cat(line, sep = "\n")
   }
-  cat("\n```")
+  cat("```\n")
 }
 
